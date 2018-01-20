02e58a7a5a128a7b431032e5edcd7fe96568e57b
zhuweiyou <i@zhuweiyou.com>
2018-01-20 08:18:54 +0800

    调整 router

--- src/router.js
+++ src/router.js
@@ -3,18 +3,19 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const asyncComponent = name => async resolve => resolve(await import(`./page/${name}.vue`))
+// const loadComponent = name => async resolve => resolve(await import(`./page/${name}.vue`))
+const loadComponent = name => resolve => resolve(require(`./page/${name}.vue`))
 
 const router = new VueRouter({
   mode: 'history',
   routes: [
     {
       path: '/',
-      component: asyncComponent('index')
+      component: loadComponent('index')
     },
     {
       path: '*',
-      component: asyncComponent('404')
+      component: loadComponent('404')
     }
   ]
 })
